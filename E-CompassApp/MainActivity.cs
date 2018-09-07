67d5bc6d9b2e054fa65ac8b654363d8251777b99
songeziwe <nciwenisongeziwe@gmail.com>
2018-09-07 11:50:07 +0200

    Main activity

--- E-CompassApp/MainActivity.cs
+++ E-CompassApp/MainActivity.cs
@@ -1,18 +1,44 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
 
 namespace E_CompassApp
 {
     [Activity(Label = "E_CompassApp", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        private Button btnLogin;
+
+        private EditText txtUsername;
+        private EditText txtPassword;
+        public object ToastLenght { get; private set; }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            SetContentView(Resource.Layout.Login);
+
+            btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+            txtUsername = FindViewById<EditText>(Resource.Id.txtUsername);
+            txtPassword = FindViewById<EditText>(Resource.Id.txtPassword);
+
+            btnLogin.Click += btnLogin_Click;
+
+        }
+
+        private void btnLogin_Click(object sender,System. EventArgs e)
+        {
+            if (txtUsername.Text == "software" && txtPassword.Text == "dev")
+            {
+                StartActivity(typeof(MainActivity));
+
+            }
+            else
+            {
+                Toast.MakeText(this, "Username or password is incorrect", ToastLength.Long).Show();
+            }
         }
     }
 }
