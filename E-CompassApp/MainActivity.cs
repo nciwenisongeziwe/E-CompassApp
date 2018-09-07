5a991d1e413a2ab572b220fc13ba4a0057cc5676
songeziwe <nciwenisongeziwe@gmail.com>
2018-09-07 12:59:27 +0200

    Login Activity

--- E-CompassApp/MainActivity.cs
+++ E-CompassApp/MainActivity.cs
@@ -32,7 +32,7 @@ namespace E_CompassApp
         {
             if (txtUsername.Text == "software" && txtPassword.Text == "dev")
             {
-                StartActivity(typeof(MainActivity));
+                StartActivity(typeof(LoginActivity));
 
             }
             else
