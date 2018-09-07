92c75cb4dd57325a4f04ab57f0328d41c42da913
songeziwe <nciwenisongeziwe@gmail.com>
2018-09-07 10:35:30 +0200

    Add project files.

---
+++ E-CompassApp/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace E_CompassApp
+{
+    [Activity(Label = "E_CompassApp", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
