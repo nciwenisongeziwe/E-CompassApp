8c35bae2901b6a499f4066d18f69d6db191a2f7d
asiphesiqangwe <asiphesiqangwe81@gmail.com>
2018-09-10 12:52:19 +0200

    a

--- E-CompassApp/Resources/Resource.Designer.cs
+++ E-CompassApp/Resources/Resource.Designer.cs
@@ -105,8 +105,8 @@ namespace E_CompassApp
 			// aapt resource value: 0x7f050002
 			public const int imageView1 = 2131034114;
 			
-			// aapt resource value: 0x7f05000c
-			public const int imageView2 = 2131034124;
+			// aapt resource value: 0x7f05000d
+			public const int imageView2 = 2131034125;
 			
 			// aapt resource value: 0x7f050001
 			public const int linearLayout1 = 2131034113;
@@ -114,17 +114,20 @@ namespace E_CompassApp
 			// aapt resource value: 0x7f050000
 			public const int linearLayout2 = 2131034112;
 			
-			// aapt resource value: 0x7f05000e
-			public const int listProductSpecials = 2131034126;
+			// aapt resource value: 0x7f05000f
+			public const int listProductSpecials = 2131034127;
 			
-			// aapt resource value: 0x7f05000d
-			public const int myImageViewText = 2131034125;
+			// aapt resource value: 0x7f05000e
+			public const int myImageViewText = 2131034126;
 			
 			// aapt resource value: 0x7f050008
 			public const int textView1 = 2131034120;
 			
 			// aapt resource value: 0x7f050006
 			public const int txtPassword = 2131034118;
+			
+			// aapt resource value: 0x7f05000c
+			public const int txtSpecials = 2131034124;
 			
 			// aapt resource value: 0x7f050005
 			public const int txtUsername = 2131034117;
