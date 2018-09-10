b9c6d30f553a4e8fb9149a26daf411191c8ae59f
asiphesiqangwe <asiphesiqangwe81@gmail.com>
2018-09-10 10:53:44 +0200

    try

--- E-CompassApp/Resources/Resource.Designer.cs
+++ E-CompassApp/Resources/Resource.Designer.cs
@@ -45,19 +45,37 @@ namespace E_CompassApp
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int e_compass = 2130837504;
+			public const int ass = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int Fries = 2130837505;
+			public const int com = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int oreos1 = 2130837506;
+			public const int e_compass = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int places = 2130837507;
+			public const int eeh = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int yogurt = 2130837508;
+			public const int Fries = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int hey = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int love = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int map = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int oreos1 = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int spring = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int yogurt = 2130837514;
 			
 			static Drawable()
 			{
