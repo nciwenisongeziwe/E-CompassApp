0b62d44c2b634968982e5669c3238ac5e07ec2f5
songeziwe <nciwenisongeziwe@gmail.com>
2018-09-10 10:47:11 +0200

    backgroung image

--- E-CompassApp/Resources/Resource.Designer.cs
+++ E-CompassApp/Resources/Resource.Designer.cs
@@ -51,13 +51,22 @@ namespace E_CompassApp
 			public const int Fries = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int map = 2130837506;
+			public const int images = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int oreos1 = 2130837507;
+			public const int map = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int yogurt = 2130837508;
+			public const int oreos1 = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int sale = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int tovary = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int yogurt = 2130837511;
 			
 			static Drawable()
 			{
