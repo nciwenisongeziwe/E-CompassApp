52f5dd68bb1e89a938f80fe3378fd1bd1e7a2d94
asiphesiqangwe <asiphesiqangwe81@gmail.com>
2018-09-20 12:28:04 +0200

    Notification

---
+++ E-CompassApp/SecondActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace E_CompassApp
+{
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            // Get the count value passed to us from MainActivity:
+            var count = Intent.Extras.GetInt(MainActivity.COUNT_KEY, -1);
+
+            // No count was passed? Then just return.
+            if (count <= 0)
+            {
+                return;
+            }
+
+            // Display the count sent from the first activity:
+            SetContentView(Resource.Layout.Home);
+            var txtView = FindViewById<TextView>(Resource.Id.textView1);
+            txtView.Text = $"The nearest store is  {count} Kilometers away.";
+        
+    }
+    }
+}
