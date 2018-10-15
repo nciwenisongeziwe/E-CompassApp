488e7e2ae88971e74e0d652d41c3d76d9c27b284
Cwenga Zozo <cwengazozo@outlook.com>
2018-10-15 16:10:41 +0200

    push monday

--- E-CompassApp/SpecialsActivity.cs
+++ E-CompassApp/SpecialsActivity.cs
@@ -20,17 +20,16 @@ namespace E_CompassApp
         private static readonly EndpointAddress Endpoint =
             new EndpointAddress("http://localhost:50874/EcompassService.svc");
 
-        private localhost.EcompassService _client = new localhost.EcompassService();
-
-        //private EcompassServiceClient _client;
+        private localhost.EcompassService _client;
+                
         private TextView txtSpecials;
         private string str;
         private ListView listSpecials;  
         private PnpProducts[] Products { get; set; }
 
         public ArrayAdapter<string> ListAdapter { get; private set; }
-
         private Button btnLoad;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -45,7 +44,7 @@ namespace E_CompassApp
                 //InitializeEcompassServiceClient();
 
                 ListSpecials();
-                ListAdapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleListItem1, Products.Length);
+              // ListAdapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleListItem1, Products.Length);
 
                 btnLoad.Click += BtnLoad_Click;
             }
@@ -60,7 +59,8 @@ namespace E_CompassApp
 
         private void BtnLoad_Click(object sender, EventArgs e)
         {
-            ListSpecials();
+            //ListSpecials();
+            BuildStr();
         }
 
         private void ListSpecials()
@@ -86,7 +86,7 @@ namespace E_CompassApp
 
         void BuildStr()
         {
-
+            _client = new localhost.EcompassService();
             //str = ecompassService.SayHelloTo();
             try
             {
@@ -102,28 +102,28 @@ namespace E_CompassApp
 
         }
 
-        void InitializeEcompassServiceClient()
-        {
-            BasicHttpBinding binding = CreateBasicHttpBinding();
-           //_client = new EcompassServiceClient(binding, Endpoint);
-        }
+        //void InitializeEcompassServiceClient()
+        //{
+        //    BasicHttpBinding binding = CreateBasicHttpBinding();
+        //   //_client = new EcompassServiceClient(binding, Endpoint);
+        //}
 
 
-        static BasicHttpBinding CreateBasicHttpBinding()
-        {
-            BasicHttpBinding binding = new BasicHttpBinding
-            {
-                Name = "basicHttpBinding",
-                MaxBufferSize = 2147483647,
-                MaxReceivedMessageSize = 2147483647
-            };
+        //static BasicHttpBinding CreateBasicHttpBinding()
+        //{
+        //    BasicHttpBinding binding = new BasicHttpBinding
+        //    {
+        //        Name = "basicHttpBinding",
+        //        MaxBufferSize = 2147483647,
+        //        MaxReceivedMessageSize = 2147483647
+        //    };
 
-            TimeSpan timeout = new TimeSpan(0, 0, 30);
-            binding.SendTimeout = timeout;
-            binding.OpenTimeout = timeout;
-            binding.ReceiveTimeout = timeout;
-            return binding;
-        }
+        //    TimeSpan timeout = new TimeSpan(0, 0, 30);
+        //    binding.SendTimeout = timeout;
+        //    binding.OpenTimeout = timeout;
+        //    binding.ReceiveTimeout = timeout;
+        //    return binding;
+        //}
 
 
 
@@ -170,72 +170,5 @@ namespace E_CompassApp
 
     }
 
-    //public async Task<List<PnpProducts>> GetPnpProductsAsync()
-    //{
-    //    EcompassServiceClient client;
 
-    //List<PnpProducts> result;
-    //try
-    //{
-
-    //    result =((List<PnpProducts>) await _client.GetProductsDataAsync());
-    //    listProduct = result;
-    //}
-    //catch (Exception ex)
-    //{
-    //    Console.WriteLine(ex.Message);
-    //}
-    //var todoItems = await Task.Factory.FromAsync<ObservableCollection<client.GetProductsData>>(
-    //    ecomass.BeginGetTodoItems,
-    //    todoService.EndGetTodoItems,
-    //    null,
-    //    TaskCreationOptions.None);
-
-
-    //foreach (var item in todoItems)
-    //{
-    //    Items.Add(FromWCFServiceTodoItem(item));
-    //}
-    //...
-    // }
-
-    //            < ImageView
-    //   android: src = "@drawable/yogurt"
-    //   android: layout_width = "82.5dp"
-    //   android: layout_height = "60.5dp"
-    //   android: id = "@+id/imageView2"
-    //   android: layout_marginRight = "0.0dp"
-    //   android: layout_marginBottom = "14.0dp"
-    //   android: layout_marginTop = "0.0dp"
-    //   android: layout_marginLeft = "0.0dp" />
-
-    //< TextView
-    //   android: id = "@+id/myImageViewText"
-    //   android: layout_width = "wrap_content"
-    //   android: layout_height = "wrap_content"
-    //   android: layout_margin = "1dp"
-    //   android: gravity = "left"
-    //   android: layout_gravity = "center_vertical"
-    //   android: text = "Yogurt R29.90 2% discount"
-    //   android: textColor = "#000000" />
-
-    //< ImageView
-    //   android: src = "@drawable/fries"
-    //   android: layout_width = "74.5dp"
-    //   android: layout_height = "56.5dp"
-    //   android: id = "@+id/imageView2"
-    //   android: layout_marginRight = "0.0dp"
-    //   android: layout_marginBottom = "14.0dp"
-    //   android: layout_marginTop = "0.0dp"
-    //   android: layout_marginLeft = "0.0dp" />
-
-    //< TextView
-    //   android: id = "@+id/myImageViewText"
-    //   android: layout_margin = "1dp"
-    //   android: layout_width = "wrap_content"
-    //   android: layout_height = "wrap_content"
-    //   android: gravity = "left"
-    //   android: layout_gravity = "center_vertical"
-    //   android: text = "Fries R40.00 2% discount"
-    //   android: textColor = "#000000" />
 }
